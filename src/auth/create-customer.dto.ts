edbfdd20cca1abd0766c037030fd66f22c3059a2
diehardcoder30 <milkilegesse6@gmail.com>
2025-01-06 23:09:39 +0300

    Add files via upload

---
+++ src/auth/create-customer.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsEmail, Length, IsPhoneNumber, IsNotEmpty } from "class-validator";
+
+export class CreateCustomerDto {
+    @IsString()
+    name: string;
+
+    
+    @IsString()
+    @IsNotEmpty()
+    CustomerId: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @Length(8, undefined, { message: "Insert a password with length greater than 8 characters" })
+    password: string;
+
+    @IsPhoneNumber('ET')
+    phoneNumber: string;
+
+    role = 'customer'; // predefined
+}
